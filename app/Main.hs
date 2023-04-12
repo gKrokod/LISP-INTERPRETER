7b647484bc0f1f34db180bc15a65a0acf8d9cd04
gKrokod <t9013054362@gmail.com>
2023-04-12 14:23:08 -0700

    todo parser. todo rework end of line

--- app/Main.hs
+++ app/Main.hs
@@ -6,15 +6,20 @@ module Main (main) where
 -- import Lib
 import Text.Parsec
 import Text.Parsec.String (Parser)
+import Text.Printf
+import Text.Parsec.Char
+import Control.Monad (void)
 
-data Token = TInt Int | TDouble Double | TList [Token] | TStr String | TSymbol Char | TPlus | TMul deriving (Eq)
+data Token = TInt Int | TDouble Double | TList [Token] | TStr String | TSymbol Char | TQuote Token | TPlus | TMul deriving (Eq)
 instance Show Token where
   show x = case x of
     TInt i -> show i
+    TDouble d -> printf "%.2f" d 
+    TQuote q -> "(quote " ++ show q ++ ")"
     TList [] -> "()"
     TList xs -> mconcat ["(", tail $ concatMap ((" " ++) . show) xs, ")"]
     TStr s -> show s
-    TSymbol c -> show c
+    TSymbol c -> c : ""
     TPlus -> "+"
     TMul -> "*"
     _ -> error "token"
@@ -32,12 +37,62 @@ main = do
 --
 parseTInt :: Parser Token
 parseTInt = do
-  n <- many1 digit
-  pure $ TInt $ read n 
+  -- n <- many1 digit
+  -- pure $ TInt $ read n 
+  pure $ TInt 1
+
+parseTToken :: Parser Token
+parseTToken = lexeme $ choice [parseTList, parseTPlus, parseTMul, parseTStr, parseTQuote]
+-- parseTToken = choice [parseTList, parseTPlus, parseTMul, parseTInt, parseTDouble, parseTStr, parseTQuote, parseTList]
+
+parseTStr :: Parser Token
+parseTStr = char '\"' *> (many $ noneOf "\"" ) >>= pure . TStr
+
+parseTSymbol :: Parser Token
+parseTSymbol = anyChar >>= pure . TSymbol 
+
+parseTQuote :: Parser Token
+-- parseTQuote = char '\'' *> choice [parseTPlus, parseTMul, parseTInt, parseTDouble, parseTStr, parseTQuote, parseTList, parseTSymbol] >>= pure . TQuote
+parseTQuote = char '\'' *> choice [parseTPlus, parseTMul, parseTStr, parseTQuote, parseTList] >>= pure . TQuote
+
+parseTPlus :: Parser Token
+parseTPlus = char '+' *>  (pure $ TPlus) 
+
+parseTMul ::  Parser Token
+parseTMul = char '*' *>  (pure $ TMul) 
+
+parseTList :: Parser Token 
+parseTList = do
+  void $ lexeme $ char '('
+  -- e <- many $ lexeme $ choice [parseTPlus, parseTMul, parseTDouble, parseTInt,parseTStr, parseTQuote, parseTList, parseTSymbol]
+  e <- many $ lexeme $ choice [parseTPlus, parseTMul, parseTStr, parseTQuote, parseTList]
+  void $ lexeme $ char ')'
+  return $ TList e
+ 
+whitespace :: Parser ()
+whitespace = void $ many $ oneOf " \n\t"
+--
+--todo make for 1.0e24 form
+--make for minus number
+parseTDouble :: Parser Token
+parseTDouble =parseTInt -- lexeme $ many1 digit >>= \h -> many digit >>= \t -> pure $ TDouble $ read (h ++ "." ++ t ++ "0")
+
+-- parseTDouble = do 
+--   h <- lexeme $ many1 digit
+--   char '.'
+--   t <- many digit 
+--   pure $ TDouble $ read (h ++ "." ++ t ++ "0")
+lexeme :: Parser a -> Parser a
+lexeme p = do
+           x <- p
+           whitespace
+           return x
 
 loop :: IO ()
 loop = do
   putStr ">>> "
   inText <- getLine
-  putStrLn inText
+  case parse parseTToken "" inText of
+    Left e -> print e
+    Right p -> print p 
   loop
