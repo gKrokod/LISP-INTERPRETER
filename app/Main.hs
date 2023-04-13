8dd6eb771e9c06cbad20fdaff466db88857db4a5
gKrokod <t9013054362@gmail.com>
2023-04-13 16:34:24 +0300

    make parser. todo delete TComment? todo load code from file for parser

--- app/Main.hs
+++ app/Main.hs
@@ -9,19 +9,23 @@ import Text.Parsec.String (Parser)
 import Text.Printf
 import Text.Parsec.Char
 import Control.Monad (void)
+import Data.List (intercalate)
 
-data Token = TInt Int | TDouble Double | TList [Token] | TStr String | TSymbol Char | TQuote Token | TPlus | TMul deriving (Eq)
+data Token = TInt Int | TDouble Double | TList [Token] | TStr String |
+             TSymbol Char | TQuote Token | TPlus | TMul | TComment String deriving (Eq)
+
 instance Show Token where
   show x = case x of
     TInt i -> show i
     TDouble d -> printf "%.2f" d 
     TQuote q -> "(quote " ++ show q ++ ")"
     TList [] -> "()"
-    TList xs -> mconcat ["(", tail $ concatMap ((" " ++) . show) xs, ")"]
+    TList xs -> mconcat ["(", intercalate " " $ filter (not . null) $ map (\case {TComment _ -> ""; x -> show x}) xs, ")"]
     TStr s -> show s
     TSymbol c -> c : ""
     TPlus -> "+"
     TMul -> "*"
+    TComment _ -> ""
     _ -> error "token"
 
 d2 = TList []
@@ -35,26 +39,40 @@ main = do
 -- надо написать для каждого токена свой парсер, далее разбить свтроку по пробелам
 -- и на каждый кусок натравить парсер по очереди, последним будет символ.
 --
+parseAnyToken :: Parser Token
+parseAnyToken = choice [parseTComment, parseTPlus, parseTMul, parseTQuote, parseTNum, parseTList, parseTStr, parseTSymbol]
+
 parseTInt :: Parser Token
 parseTInt = do
-  -- n <- many1 digit
-  -- pure $ TInt $ read n 
-  pure $ TInt 1
+  n <- many1 digit
+  pure $ TInt $ read n 
+  -- pure $ TInt 1
 
 parseTToken :: Parser Token
-parseTToken = lexeme $ choice [parseTList, parseTPlus, parseTMul, parseTStr, parseTQuote]
--- parseTToken = choice [parseTList, parseTPlus, parseTMul, parseTInt, parseTDouble, parseTStr, parseTQuote, parseTList]
+parseTToken = lexeme $ parseAnyToken
 
 parseTStr :: Parser Token
-parseTStr = char '\"' *> (many $ noneOf "\"" ) >>= pure . TStr
+parseTStr = do
+  void $ char '"'
+  str <- many $ noneOf "\"" 
+  void $ char '"'
+  pure $ TStr str
+
+
+parseTComment :: Parser Token
+parseTComment = do
+  void $ char ';'
+  cmt <- many $ noneOf ";" 
+  void $ char ';'
+  pure $ TComment cmt
+-- char '"' *> (many $ noneOf "\"" ) >>= pure . TStr
 
 parseTSymbol :: Parser Token
-parseTSymbol = anyChar >>= pure . TSymbol 
+parseTSymbol = alphaNum  >>= pure . TSymbol
 
 parseTQuote :: Parser Token
--- parseTQuote = char '\'' *> choice [parseTPlus, parseTMul, parseTInt, parseTDouble, parseTStr, parseTQuote, parseTList, parseTSymbol] >>= pure . TQuote
-parseTQuote = char '\'' *> choice [parseTPlus, parseTMul, parseTStr, parseTQuote, parseTList] >>= pure . TQuote
-
+parseTQuote = char '\'' *> parseAnyToken >>= pure . TQuote
+--
 parseTPlus :: Parser Token
 parseTPlus = char '+' *>  (pure $ TPlus) 
 
@@ -64,8 +82,7 @@ parseTMul = char '*' *>  (pure $ TMul)
 parseTList :: Parser Token 
 parseTList = do
   void $ lexeme $ char '('
-  -- e <- many $ lexeme $ choice [parseTPlus, parseTMul, parseTDouble, parseTInt,parseTStr, parseTQuote, parseTList, parseTSymbol]
-  e <- many $ lexeme $ choice [parseTPlus, parseTMul, parseTStr, parseTQuote, parseTList]
+  e <- many $ lexeme $ parseAnyToken
   void $ lexeme $ char ')'
   return $ TList e
  
@@ -74,25 +91,25 @@ whitespace = void $ many $ oneOf " \n\t"
 --
 --todo make for 1.0e24 form
 --make for minus number
-parseTDouble :: Parser Token
-parseTDouble =parseTInt -- lexeme $ many1 digit >>= \h -> many digit >>= \t -> pure $ TDouble $ read (h ++ "." ++ t ++ "0")
+parseTNum :: Parser Token
+parseTNum = do 
+  h <- many1 digit
+  (char '.' *> many digit >>= \t -> pure $ TDouble $ read (h ++ "." ++ t ++ "0")) <|> (pure $ TInt $ read h)
 
--- parseTDouble = do 
---   h <- lexeme $ many1 digit
---   char '.'
---   t <- many digit 
---   pure $ TDouble $ read (h ++ "." ++ t ++ "0")
 lexeme :: Parser a -> Parser a
 lexeme p = do
            x <- p
            whitespace
            return x
 
+
 loop :: IO ()
 loop = do
   putStr ">>> "
   inText <- getLine
-  case parse parseTToken "" inText of
+  let isList = length (words inText) > 1
+  let inText' = if isList then '(' : inText ++ ")" else inText
+  case parse parseTToken "" inText' of
     Left e -> print e
     Right p -> print p 
   loop
