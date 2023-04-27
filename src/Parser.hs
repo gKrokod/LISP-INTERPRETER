5ddea6751309b2036dc4185f108995b083bc41e0
gKrokod <t9013054362@gmail.com>
2023-04-27 21:15:47 +0300

    make new Parser. todo make list without ()

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,113 +1,86 @@
-module Parser (readREPL) where
+module Parser where
 
 import Types
 import Text.Parsec
 import Text.Parsec.String (Parser)
 import Control.Monad 
 
-readREPL :: String -> Either ParseError Token
-readREPL text = makeTypeFromSymbol . filterComment <$> parse parseTToken "" text'
-  where 
-   txt = dropWhile (==' ') text
-   isList = (not . null) txt && length (words txt) > 1 && head txt /= '('
-   text' = if isList then '(' : txt ++ ")" else txt
+-- удалить комментарии, которые открываются и закрываются символом ;
+clearComment :: String -> String
+clearComment = snd . foldr f (False, [])
+  where f ';' (flag, acc) = (not flag, acc)
+        f x (False, acc) = (False, x : acc)
+        f x (True, acc) = (True, acc)
 
-filterComment :: OldToken -> Token
-filterComment (TList xs) = TList $ foldr f [] xs
-  where f (TComment _) acc = acc
-        f x acc = x : acc
-filterComment x = x 
 
-type OldToken = Token
-makeTypeFromSymbol :: OldToken -> Token
-makeTypeFromSymbol (TSymbol "+") = BO ADD
-makeTypeFromSymbol (TSymbol "-") = BO SUB
-makeTypeFromSymbol (TSymbol "*") = BO MUL
-makeTypeFromSymbol (TSymbol "/") = BO DIV
-makeTypeFromSymbol (TSymbol "mod") = BO MOD
-makeTypeFromSymbol (TSymbol "++") = BO CONCAT
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=?>@^_~#" 
 
-makeTypeFromSymbol (TSymbol ">") = BP GT'
-makeTypeFromSymbol (TSymbol "<") = BP LT'
-makeTypeFromSymbol (TSymbol "==") = BP EQ'
-
-makeTypeFromSymbol (TSymbol "def") = SF DEF
-makeTypeFromSymbol (TSymbol "set!") = SF SET
-makeTypeFromSymbol (TSymbol "get") = SF GET
-makeTypeFromSymbol (TSymbol "quote") = SF QUOTE
-makeTypeFromSymbol (TSymbol "typeof") = SF TYPEOF
-makeTypeFromSymbol (TSymbol "cons") = SF CONS
-makeTypeFromSymbol (TSymbol "car") = SF CAR
-makeTypeFromSymbol (TSymbol "cdr") = SF CDR
-makeTypeFromSymbol (TSymbol "cond") = SF COND
-makeTypeFromSymbol (TSymbol "print") = SF PRINT
-makeTypeFromSymbol (TSymbol "read") = SF READ
-makeTypeFromSymbol (TSymbol "eval") = SF EVAL
-makeTypeFromSymbol (TSymbol "eval-in") = SF EVALIN
-makeTypeFromSymbol (TSymbol "lambda") = SF LAMBDA
-makeTypeFromSymbol (TSymbol "macro") = SF MACRO
-makeTypeFromSymbol (TSymbol "macroexpand") = SF MACROEXPAND
-makeTypeFromSymbol (TList xs) = TList $ map makeTypeFromSymbol xs
--- makeTypeFromSymbol (TSymbol "symbol") = SF SYMBOL  -- дублирует наверно тип TSymbol String
-makeTypeFromSymbol xs = xs
-
-parseAnyToken :: Parser Token
-parseAnyToken = choice [parseTComment, parseTQuote, parseTNum, parseTList, parseTStr, parseTSymbol]
-
-parseTInt :: Parser Token
-parseTInt = do
-  n <- many1 digit
-  pure $ TInt $ read n 
-
-parseTToken :: Parser Token
-parseTToken = lexeme $ parseAnyToken
-
-parseTStr :: Parser Token
-parseTStr = do
-  void $ char '"'
-  str <- many $ noneOf "\"" 
-  void $ char '"'
-  pure $ TStr str
-
-parseTComment :: Parser Token
-parseTComment = do
-  void $ char ';'
-  cmt <- many $ noneOf ";" 
-  void $ char ';'
-  pure $ TComment cmt
-
-parseTSymbol :: Parser Token
-parseTSymbol = many1 ( noneOf ("() \n\t\"';" ++ ['0'.. '9'])) >>= pure . TSymbol
-
-parseTQuote :: Parser Token
-parseTQuote = char '\'' *> parseAnyToken >>= pure . TQuote
---
--- parseTPlus :: Parser Token
--- -- parseTPlus = char '+' *>  (pure $ TPlus) 
--- parseTPlus = char ',' *>  (pure $ TPlus) 
---
--- parseTMul ::  Parser Token
--- parseTMul = char '*' *>  (pure $ TMul) 
-
-parseTList :: Parser Token 
-parseTList = do
-  void $ lexeme $ char '('
-  e <- many $ lexeme $ parseAnyToken
-  void $ lexeme $ char ')'
-  return $ TList e
- 
 whitespace :: Parser ()
 whitespace = void $ many $ oneOf " \n\t"
---
---todo make for 1.0e24 form
---make for minus number
-parseTNum :: Parser Token
-parseTNum = do 
-  h <- many1 digit
-  (char '.' *> many digit >>= \t -> pure $ TDouble $ read (h ++ "." ++ t ++ "0")) <|> (pure $ TInt $ read h)
 
 lexeme :: Parser a -> Parser a
 lexeme p = do
            x <- p
            whitespace
            return x
+
+parseNumber :: Parser SExpr
+parseNumber = (char '-' >> many1 digit >>= pure . Number . read . ("-" ++))
+              <|> (many1 digit >>= pure . Number . read)
+
+parseString :: Parser SExpr
+parseString = do
+  void $ char '"'
+  str <- many $ noneOf "\""
+  void $ char '"'
+  pure $ String str
+
+
+parseList :: Parser SExpr 
+parseList = do
+  void $ char '('
+  e <- many $ lexeme $ parseAnySExpr
+  void $ char ')'
+  return $ List e
+
+parseAtom :: Parser SExpr
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = [first] ++ rest
+  pure $ case atom of
+    "#t" -> Bool True 
+    "#f" -> Bool False
+    "def" -> SForm DEF
+    "set!" -> SForm SET
+    "get" -> SForm GET
+    "type-of" -> SForm TYPEOF
+    "cons" -> SForm CONS
+    "car" -> SForm CAR
+    "cdr" -> SForm CDR
+    "cond" -> SForm COND
+    "if" -> SForm IF
+    "read" -> SForm READ
+    "print" -> SForm PRINT
+    "eval" -> SForm EVAL
+    "eval-in" -> SForm EVALIN
+    "l" -> SForm LAMBDA
+    "macro" -> SForm MACRO
+    "+" -> BOper ADD
+    "-" -> BOper SUB
+    "*" -> BOper MUL
+    ">" -> BPrim GT'
+    "<" -> BPrim LT'
+    "==" -> BPrim EQ'
+    otherwise -> Atom atom
+
+parseQuoted :: Parser SExpr
+parseQuoted = char '\'' *> parseAnySExpr >>= \x -> pure $ List [Atom "quote", x] 
+
+parseAnySExpr :: Parser SExpr
+parseAnySExpr = do
+  whitespace
+  choice [parseAtom, parseString, parseNumber, parseQuoted, parseList]
+
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,34 @@
+import Types 
+import Parser
+
+import Test.Hspec (context, describe, hspec, it, shouldBe, shouldNotBe)
+import Test.Hspec.QuickCheck (modifyMaxSuccess)
+import Test.QuickCheck (property)
+import Text.Parsec
+import Data.Either
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Bot logic" $ modifyMaxSuccess (const 1000) $ do
+    context "Parser Number" $ do
+      it "Input: Int" $ do
+        property $ \int -> do
+          fromRight (Number 0) (parse parseNumber "lisp" (show int))
+            `shouldBe`  Number int 
+
+      it "Input: Str" $ do
+        property $ \str -> do
+          let clearStr = filter (/='\"') str
+          fromRight (String "") (parse parseString "lisp" (concat["\"", clearStr, "\""]) )
+            `shouldBe` String clearStr 
+
+      it "Input: Symbol" $ do
+        property $ \str -> do
+          let firstSymbol = "!$%&|*+-/:<=?>@^_~#" ++ ['A'..'Z'] ++ ['a'..'z'] 
+          let restSymbol = firstSymbol ++ ['0'..'9']
+          let str' = if null str then "a"
+                     else if head str `elem` firstSymbol
+                       then [head str] ++ filter (`elem` restSymbol) (tail str)
+                       else "a"
+          fromRight (Atom "") (parse parseAtom "lisp" str') 
+            `shouldBe` Atom str' 
